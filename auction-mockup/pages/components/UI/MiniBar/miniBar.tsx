796d27a7a455bdd0f5eccdd40d15093dbafdaf22
LRboice <lucasrboice@gmail.com>
2025-01-28 15:53:11 -0600

    - restructuring to reflect minibar component

--- auction-mockup/pages/components/UI/MiniBar/miniBar.tsx
+++ auction-mockup/pages/components/UI/MiniBar/miniBar.tsx
@@ -6,11 +6,11 @@ const MiniBar: React.FC = () => {
         <div className={styles.miniBar}> 
             <button className={styles.button}> Categories </button>
             <button className={styles.button}> Model Specific </button>
-            <button className={styles.button}> Shipping </button>
-            <button className={styles.button}> Notify Me </button>
+            <button className={styles.button}> Shipping </button> 
             <button className={styles.button}> Local Events </button>
             <button className={styles.button}> Checkout </button>
             <button className={styles.button}> About Us </button>
+            <button className={styles.button}> User Settings </button>
         </div>
     ); 
 
